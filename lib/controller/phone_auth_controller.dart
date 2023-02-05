74b446945e7f6cf85ba3b96ef11f04be378201a1
Nasirjavaid <farhanaliyt@gmail.com>
2023-02-05 14:34:35 +0500

    send sms using twilio verify otp

--- lib/controller/phone_auth_controller.dart
+++ lib/controller/phone_auth_controller.dart
@@ -1,20 +1,55 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
+import 'package:twilio_flutter/twilio_flutter.dart';
 
-class PhoneAuthController extends GetxController{
-  TextEditingController phoneController=TextEditingController();
-  TextEditingController codeController=TextEditingController();
+class PhoneAuthController extends GetxController {
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController codeController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  bool isCodeSent = false;
 
+  late TwilioFlutter twilioFlutter;
 
-  final formKey=GlobalKey<FormState>();
+  var sentOTP;
 
-  bool isCodeSent=false;
-
-
-  showInvisibleWgts(){
-    isCodeSent=true;
+  showInvisibleWidgets() {
+    isCodeSent = true;
     update();
   }
 
+  sendSMS() {
+    twilioFlutter = TwilioFlutter(
+        accountSid: '', // replace it with your account SID
+        authToken: '', // replace it with your auth token
+        twilioNumber: '' // replace it with your purchased twilioNumber
 
+        );
+
+    var rnd = new Random();
+
+    var digits = rnd.nextInt(900000) + 100000;
+
+    sentOTP = digits;
+
+    // lets print otp as well
+
+    print(sentOTP);
+
+    twilioFlutter.sendSMS(
+        toNumber: phoneController.text,
+        messageBody: 'Hello This is 6 digit otp code to verify phone $digits');
+  }
+
+  verifyOTP() {
+    if (sentOTP.toString() == codeController.text) {
+      Fluttertoast.showToast(
+          msg: "OTP Verified SuccessFully!", backgroundColor: Colors.green);
+    } else {
+      Fluttertoast.showToast(
+          msg: "OTP didn't match!", backgroundColor: Colors.red);
+    }
+  }
 }
--- lib/screen/phone_auth_screen.dart
+++ lib/screen/phone_auth_screen.dart
@@ -3,126 +3,100 @@ import 'package:get/get.dart';
 import 'package:phone_auth_project/controller/phone_auth_controller.dart';
 
 class PhoneAuthScreen extends StatelessWidget {
-  // to use controller we need to create an instance of that controller here before the build method
-
-  PhoneAuthController controller=Get.put(PhoneAuthController());
+  PhoneAuthController controller = Get.put(PhoneAuthController());
 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Phone Auth"),
-        centerTitle: true,
-        elevation: 0,
-      ),
-
-      body:
-      // we will wrap body widget with get builder to manage the state using controller
-
-    GetBuilder<PhoneAuthController>(
-      builder: (_)=>
-
-      Padding(
-        padding: EdgeInsets.all(16),
-        // lets add form for phone and code auth
-        child: Form(
-          key: controller.formKey,
-
-          child: Column(
-            // we want all these widgets to show in center of screen so
-
-            mainAxisAlignment: MainAxisAlignment.center,
-
-            children: [
-            //we need to add two textfields 1. one for the phone number
-            //2. for code authentication
-
-            TextFormField(
-              controller: controller.phoneController,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                //lets add hint and lebel here
-                hintText: "03472222",
-                labelText: "Phone Number"
-              ),
-
-              // lets add validation
-              validator: (String? number){
-                if(number!.isEmpty){
-                  return "Enter Phone Number";
-                }
-                return null;
-              },
-            ),
-              SizedBox(height: 10,),
-
-            // same we need to add code text field for phone number auhtenictaion
-            Visibility(
-              visible: controller.isCodeSent,
-              child: TextFormField(
-                controller: controller.codeController, // we need to create controller
-                keyboardType: TextInputType.number,
-                maxLength: 6,
-                decoration: InputDecoration(
-                  //lets add hint and lebel here
-                    hintText: "123456",
-                    labelText: "Code"
-                ),
-
-                // lets add validation
-                validator: (String? code){
-                  if(code!.isEmpty){
-                    return "Enter Code";
-                  }else if(code.length<6){
-                    return "Code should be of length 6";
-                  }
-
-                  return null;
-                },
-              ),
-            ),
-
-            SizedBox(height: 10,),
-
-            // now we need to create a button here to submit the form
-
-            Visibility(
-              visible: !controller.isCodeSent,
-              child: Container(
-                  // we want button to take the same width as the textfield is taking so
-                width: MediaQuery.of(context).size.width,
-                  child: ElevatedButton(onPressed: (){
-
-                    if(controller.formKey.currentState!.validate()){
-
-                      //it will show the code field and verify button on submit tap
-                      // but we have to hide submit button then so lets implement that
-                      controller.showInvisibleWgts();
-                    }
-
-                  }, child: Text('Submit'))),
-            ),
-
-
-            Visibility(
-              visible: controller.isCodeSent,
-              child: Container(
-                  // we want button to take the same width as the textfield is taking so
-                width: MediaQuery.of(context).size.width,
-                  child: ElevatedButton(onPressed: (){
-
-                    if(controller.formKey.currentState!.validate()){
-                      // we will implement functionality here later
-
-                      // lets test the form validation
-                    }
-
-                  }, child: Text('Verify'))),
-            ),
-          ],),
+        appBar: AppBar(
+          title: const Text("Phone Auth"),
+          centerTitle: true,
+          elevation: 0,
         ),
-      ),
-    ));
+        body: GetBuilder<PhoneAuthController>(
+          builder: (_) => Padding(
+            padding: const EdgeInsets.all(16),
+            child: Form(
+              key: controller.formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextFormField(
+                    controller: controller.phoneController,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                        hintText: "03472222", labelText: "Phone Number"),
+
+                    // lets add validation
+                    validator: (String? number) {
+                      if (number!.isEmpty) {
+                        return "Enter Phone Number";
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Visibility(
+                    visible: controller.isCodeSent,
+                    child: TextFormField(
+                      controller: controller
+                          .codeController,
+                      keyboardType: TextInputType.number,
+                      maxLength: 6,
+                      decoration: const InputDecoration(
+                          hintText: "123456",
+                          labelText: "Code"),
+
+                      // lets add validation
+                      validator: (String? code) {
+                        if (code!.isEmpty) {
+                          return "Enter Code";
+                        } else if (code.length < 6) {
+                          return "Code should be of length 6";
+                        }
+
+                        return null;
+                      },
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Visibility(
+                    visible: !controller.isCodeSent,
+                    child: SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        child: ElevatedButton(
+                            onPressed: () {
+                              if (controller.formKey.currentState!.validate()) {
+
+                                controller.sendSMS();
+                                controller.showInvisibleWidgets();
+                              }
+                            },
+                            child: const Text('Submit'))),
+                  ),
+                  Visibility(
+                    visible: controller.isCodeSent,
+                    child: SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        child: ElevatedButton(
+                            onPressed: () {
+                              if (controller.formKey.currentState!
+                                  .validate()) {
+
+                                controller.verifyOTP();
+                              }
+                            },
+                            child: const Text('Verify'))),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
